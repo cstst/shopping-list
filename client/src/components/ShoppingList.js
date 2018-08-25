f6afb509fb68220a4350ec0e2958b9173a6148a4
unknown <derekslarson@gmail.com>
2018-08-25 09:50:38 -0700

    list done

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -23,13 +23,35 @@ export default class ShoppingList extends Component {
                     onClick={() => {
                         const name = prompt('Enter Item');
                         if (name) {
-                            this.setState({
-                                items: [...items, { id: uuid(), name }]
-                            });
+                            this.setState(state => ({
+                                items: [...state.items, { id: uuid(), name }]
+                            }));
                         }
                     }}
                 >Add Item</Button>
-                
+                <ListGroup>
+                    <TransitionGroup className="shopping-list">
+                        {items.map(({ id, name }) => (
+                            <CSSTransition key={id} timeout={500} classNames="fade">
+                                <ListGroupItem>
+                                    <Button
+                                        className="remove-btn"
+                                        color="danger"
+                                        size="sm"
+                                        onClick={() => {
+                                            this.setState(state => ({
+                                                items: state.items.filter(item => item.id !== id)
+                                            }));
+                                        }}   
+                                    >
+                                        &times;
+                                    </Button>
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
             </Container>
         )
     }
